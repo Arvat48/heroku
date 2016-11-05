82214c405b621f81195eedcab8598040a6d3fe73
LAPTOP-V3AU0STS\antoi <antoine.bonneau65@gmail.com>
2016-11-05 11:30:45 +0100

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularHerokuApp
+ * @description
+ * # angularHerokuApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularHerokuApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularHerokuApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularHerokuApp
+ */
+angular.module('angularHerokuApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
